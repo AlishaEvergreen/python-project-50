c4f7c7ec19f49000e1998224e2281fea12e2c4d0
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-09-22 20:10:46 +0300

    Add tests to compare flat JSON files

---
+++ gendiff/tests/test_gendiff.py
@@ -0,0 +1,23 @@
+import pytest
+from gendiff import generate_diff
+from pathlib import Path
+
+BASE_PATH = Path(__file__).parent / 'fixtures'
+
+
+def read_file(filepath):
+    with open(filepath, encoding='utf8') as f:
+        return f.read().strip()
+
+
+def get_fixture_path(filename):
+    return BASE_PATH / filename
+
+
+@pytest.mark.parametrize("file1, file2, expected", [
+    (get_fixture_path('file1.json'),
+     get_fixture_path('file2.json'),
+     read_file(get_fixture_path('flat_json_result.txt')))
+])
+def test_gendiff(file1, file2, expected):
+    assert generate_diff(file1, file2) == expected
