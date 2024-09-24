8a9f13c30b018862346a7bc46fb5e85085107f9d
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-09-24 21:16:40 +0300

    Add tests to check the functionality of comparing YML and YAML files

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -15,7 +15,10 @@ def read_file(filepath):
 @pytest.mark.parametrize("file1, file2, expected", [
     (get_fixture_path('file1.json'),
      get_fixture_path('file2.json'),
-     read_file(get_fixture_path('flat_json_result.txt')))
+     read_file(get_fixture_path('flat_json_yml_result.txt'))),
+    (get_fixture_path('file1.yml'),
+     get_fixture_path('file2.yml'),
+     read_file(get_fixture_path('flat_json_yml_result.txt'))),
 ])
 def test_gendiff(file1, file2, expected):
     assert generate_diff(file1, file2) == expected
