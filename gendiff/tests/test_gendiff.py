669c6f34d16bce25201b07ee3eb61459cb37fb6b
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-09-24 21:53:59 +0300

    Add tests to check the functionality of comparing JSON/YML/YAML files with nesting

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -19,6 +19,12 @@ def read_file(filepath):
     (get_fixture_path('file1.yml'),
      get_fixture_path('file2.yml'),
      read_file(get_fixture_path('flat_json_yml_result.txt'))),
+    (get_fixture_path('nested_file1.json'),
+     get_fixture_path('nested_file2.json'),
+     read_file(get_fixture_path('nested_json_yaml_result.txt'))),
+    (get_fixture_path('nested_file1.yaml'),
+     get_fixture_path('nested_file2.yaml'),
+     read_file(get_fixture_path('nested_json_yaml_result.txt'))),
 ])
 def test_gendiff(file1, file2, expected):
     assert generate_diff(file1, file2) == expected
