58d09c00afa5f8b7276a1e4dd2741b88f5b190f0
Alina Makarovskaya <makarovskaya1307@gmail.com>
2024-10-05 20:11:36 +0300

    Add tests to check the functionality of plain output

--- gendiff/tests/test_gendiff.py
+++ gendiff/tests/test_gendiff.py
@@ -12,19 +12,30 @@ def read_file(filepath):
         return f.read().strip()
 
 
-@pytest.mark.parametrize("file1, file2, expected", [
+@pytest.mark.parametrize("file1, file2, format, expected, ", [
     (get_fixture_path('file1.json'),
-     get_fixture_path('file2.json'),
+     get_fixture_path('file2.json'), 'stylish',
      read_file(get_fixture_path('flat_json_yml_result.txt'))),
+
     (get_fixture_path('file1.yml'),
-     get_fixture_path('file2.yml'),
+     get_fixture_path('file2.yml'), 'stylish',
      read_file(get_fixture_path('flat_json_yml_result.txt'))),
+
     (get_fixture_path('nested_file1.json'),
-     get_fixture_path('nested_file2.json'),
-     read_file(get_fixture_path('nested_json_yaml_result.txt'))),
+     get_fixture_path('nested_file2.json'), 'stylish',
+     read_file(get_fixture_path('stylish_result.txt'))),
+
     (get_fixture_path('nested_file1.yaml'),
-     get_fixture_path('nested_file2.yaml'),
-     read_file(get_fixture_path('nested_json_yaml_result.txt'))),
+     get_fixture_path('nested_file2.yaml'), 'stylish',
+     read_file(get_fixture_path('stylish_result.txt'))),
+
+    (get_fixture_path('nested_file1.json'),
+     get_fixture_path('nested_file2.json'), 'plain',
+     read_file(get_fixture_path('plain_result.txt'))),
+
+    (get_fixture_path('nested_file1.yaml'),
+     get_fixture_path('nested_file2.yaml'), 'plain',
+     read_file(get_fixture_path('plain_result.txt'))),
 ])
-def test_gendiff(file1, file2, expected):
-    assert generate_diff(file1, file2) == expected
+def test_gendiff(file1, file2, format, expected):
+    assert generate_diff(file1, file2, format) == expected
